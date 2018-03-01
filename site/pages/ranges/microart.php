27df508122be03a7669ad7ed4f61d3603daf6119
Siobhan Depper <trolltrader@googlemail.com>
2018-03-01 13:03:55 +0000

    1.4.1; added microart

---
+++ site/pages/ranges/microart.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Micro Art Studio
+ */
+?>
+
+<?php
+  require_once $_SERVER['DOCUMENT_ROOT'] . ('/Kingsley_Distribution/site/parts/variables.php');
+?>
+
+<!-- Header -->
+<?php include $root_doc . ('parts/header/header.php'); ?>
+
+<!-- Introduction -->
+<div class="microart__about">
+  <div class="microart__about--txt">
+    <div class="letter-a">
+      <h2>About</h2>
+    </div>
+    <div>
+      <p>Micro Art Studio are known as one of the largest and best manufacturers of wargames accessories, bases and scenery.</p>
+      <p>With repeated success in Kickstarter campains, Micro Art Studio have been able to bring their skirmish tabletop game to life as well as provide the wargaming community with a spectacular range of polyurethane foam terrain.</p>
+    </div>
+  </div>
+</div>
+
+<!-- Bases -->
+<div class="microart__bases">
+  <div class="microart__bases--container">
+    <div class="microart__bases--img">
+      <picture>
+        <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_bases_01.webp" type="image/webp">
+        <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_bases_01.jpg"  alt="microart bases">
+      </picture>
+    </div>
+    <div class="microart__bases--txt">
+      <div class="letter-b">
+        <h2>Bases</h2>
+      </div>
+      <div>
+        <p>After the success of the successful Kickstarter campaign where Microart and CoolMiniOrNot teamed up to create the next generation of miniature bases, Microart now offer an extensive amount of resin bases ranging is a vast number of different sizes to enable every type of wargamer the opportunity to purchase beautifully crafted resin bases for their miniatures.</p>
+        <p>Bases come in two parts, the first piece is a highly detailed terrain insert. The second piece is a black satin base, with a recessed center that holds the insert. This two part system creates a cohesive and beautiful base for your miniatures.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Discworld -->
+<div class="microart__discworld">
+  <div class="microart__discworld--container">
+    <div class="microart__discworld--txt">
+      <div class="letter-d">
+        <h2>Discworld</h2>
+      </div>
+      <div>
+        <p>Based on the unmistakeable designs of Paul Kidby, Microart have fantastically bought the classic works of Sir Terry Pratchett to life through the the officially licensed Discworld miniature series.</p>
+        <p>The comic fantasy book series is based on the fictional Discworld, a flat disc which balances on the backs of four elephants which in turn stand on the back of a giant turtle known as the Great A'Tuin. The Discworld book series has succeeded in capturing the imaginations of millions of people since the first book in 1983.</p>
+        <p>Microart have done the series proud with their beautifully crafted metal miniatures. Each and every Discworld miniature is oozing with character and made to an extremely high quality at 30mm scale. These stunning miniatures will not look out of place alongside any fan's Discworld collection.</p>
+      </div>
+    </div>
+    <div class="microart__discworld--img">
+      <picture>
+        <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_discworld_01.webp" type="image/webp">
+        <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_discworld_01.jpg"  alt="microart bases">
+      </picture>
+    </div>
+  </div>
+</div>
+
+<!-- Designed for Infinity -->
+<div class="microart__infinity">
+  <div class="microart__infinity--txt">
+    <div class="letter-i">
+      <h2>Designed for Infinity</h2>
+    </div>
+    <div>
+      <p>Infinity is a tabletop wargame in which sci-fi themed miniatures are used to simulate futuristic skirmishes; it is a groundbreaking, dynamic system that allows you to make meaningful, fun choices throughout the entire game sequence, and gives you the tools to implement any number of strategies with realism and flexibility.</p>
+      <p>With the Designed for Infinity series by Microart, you can build your own unique </p>
+      <p>All Designed for Infinity products are designed in collaboration with Corvus Belli specifically for Infinity The Game.</p>
+    </div>
+  </div>
+</div>
+<div class="microart__infinity--img">
+  <picture>
+    <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_01.webp" type="image/webp">
+    <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_01.jpg" alt="corvus belli infinity miniatures">
+  </picture>
+  <picture>
+    <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_02.webp" type="image/webp">
+    <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_02.jpg" alt="corvus belli infinity miniatures">
+  </picture>
+  <picture>
+    <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_03.webp" type="image/webp">
+    <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_03.jpg" alt="corvus belli infinity miniatures">
+  </picture>
+</div>
+
+<!-- Scenery -->
+<div class="microart__scen">
+  <div class="microart__scen--txt">
+    <div class="letter-b">
+      <h2>Scenery</h2>
+    </div>
+    <div>
+      <p>There are a couple of varieties in Microart scenery to choose from. There is beautifully cast resin pieces, hard-wearing and highly impressive HDF buildings, and finally lightweight but durable hardfoam.</p>
+      <p>Resin scenery is fantastic due to the fact that it can contain more detail per piece that isn't as achieveable with HDF or Hardfoam.</p>
+      <p>HDF stands for High Density Firbreboard; most people are familiar with MDF, HDF is very similar except that is it denser and more versatile than MDF. The additions of laser etched details really add depth and detail to enhance the finished, painted product.</p>
+      <p>Hard Foam is amazing because it's lightweight, durable and easy to cut with a knife to make any alterations to add a uniqueness to your scenery that sets it apart from everyone else's.</p>
+      <p>All scenery will work well with any 28mm scale miniature game.</p>
+    </div>
+  </div>
+  <div class="microart__scen--img">
+    <picture>
+      <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_scenery_01.webp" type="image/webp">
+      <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/mircroart_scenery_01.jpg" alt="microart scenery">
+    </picture>
+  </div>
+</div>
+
+<!-- Mats -->
+<div class="microart__mats">
+  <div class="microart__mats--txt">
+    <div class="letter-m">
+      <h2>Mats</h2>
+    </div>
+    <div>
+      <p>Microart Studio mats are like a work of art; they are beautiful. But their beauty doesn't compromise their durability. Microart Studio mats are highly praised for the added quality the art brings to the gaming table all the while being able to stand up against continuous heavy use.</p>
+      <p>All mats are made with a polyester and rubber mixture. The top half of the mat is a high quality printout on polyester surface, while the bottom half is made out of rubber to prevent it slipping on a table surface. You may find that it is similar to "mousepad" material.</p>
+    </div>
+  </div>
+</div>
+<div class="microart__mats--img">
+  <picture>
+    <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_mat_01.jpg" type="image/webp">
+    <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_mat_01.jpg" alt="corvus belli infinity miniatures">
+  </picture>
+  <picture>
+    <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_mat_02.webp" type="image/webp">
+    <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_mat_02.jpg" alt="corvus belli infinity miniatures">
+  </picture>
+  <picture>
+    <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_mat_03.webp" type="image/webp">
+    <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_mat_03.jpg" alt="corvus belli infinity miniatures">
+  </picture>
+</div>
+
+<!-- WWX -->
+<div class="microart__wwx">
+  <div class="microart__wwx--txt">
+    <div class="letter-w">
+      <h2>Wild West Exodus</h2>
+    </div>
+    <div>
+      <p>Wild West Exodus is a dynamic, fast-paced tabletop miniatures game pitting a cast of heroic characters against each other in a brutal world - very much a twisted reflection of our own.</p>
+      <p>No Ninteenth Centuary battleground would be complete without the impressive HDF buildings and hardfoam scenery pieces expertly designed at Microart.</p>
+    </div>
+  </div>
+  <div class="microart__wwx--img">
+    <picture>
+      <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_wwx_01.webp" type="image/webp">
+      <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_wwx_01.jpg" alt="microart scenery">
+    </picture>
+  </div>
+</div>
+
+<!-- Call to Action -->
+<div class="call-to">
+  <div class="letter-o">
+    <h2>Place an Order Now</h2>
+  </div>
+  <p></p>
+  <a class="btn__order--ylw" href="mailto:sales@kingsleydistribution.co.uk?subject=Order: Micro Art Studios">Make an Order</a>
+</div>
+
+<!-- Art -->
+<div class="microart__art">
+  <picture>
+    <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_05.jpg" type="image/webp">
+    <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_05.jpg" alt="corvus belli infinity miniatures">
+  </picture>
+  <picture>
+    <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_07.webp" type="image/webp">
+    <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_07.jpg" alt="corvus belli infinity miniatures">
+  </picture>
+  <picture>
+    <source data-srcset="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_06.webp" type="image/webp">
+    <img data-src="https://res.cloudinary.com/kingsley/image/upload/q_auto/ranges/microart/microart_infinity_06.jpg" alt="corvus belli infinity miniatures">
+  </picture>
+</div>
+
+<!-- Footer -->
+<?php include $root_doc . ('parts/footer/footer.php'); ?>
